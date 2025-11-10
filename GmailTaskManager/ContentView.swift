e5bb221ab999ee629203dcf6b5ce20356620d0ac
homieyoung <yjgwon@mz.co.kr>
2025-11-10 10:16:38 +0900

    Initial Commit

---
+++ GmailTaskManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GmailTaskManager
+//
+//  Created by MZC01-YJGWON on 11/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GmailTaskManager/GmailTaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GmailTaskManagerApp.swift
+//  GmailTaskManager
+//
+//  Created by MZC01-YJGWON on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GmailTaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
